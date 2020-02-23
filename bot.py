8dec5cca12dc45507bab33026bb7bac855a4c4dd
Kirill Almazov <velmir.slav@gmail.com>
2020-02-23 20:13:34 +0300

    Первый бот. Курс питона

---
+++ bot.py
@@ -0,0 +1,37 @@
+# Импортирт компонентов
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import settings
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+# Ответ на старт
+def greet_user(bot, update):
+    text = "вызван /start"
+    update.message.reply_text(text)
+
+# Эхо бот
+def talk_to_me(bot, update):
+    user_text = "Привет! {}: Ты написал: {}".format(update.message.chat.first_name, update.message.text)
+    logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username,
+                 update.message.chat.id, update.message.text)
+    update.message.reply_text(user_text)
+
+# Функция, которая соединяется с платформой Telegram, "тело" нашего бота
+def main():
+    mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
+
+    logging.info('Бот запускается')
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    mybot.start_polling()
+    mybot.idle()
+
+
+main()
